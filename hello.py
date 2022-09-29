d1cf361ecf0f68d4ae80d710105684d6038a046f
Kevin Mi <mikevin920@yahoo.com>
2022-09-28 20:08:26 -0400

    Completed Activity 3

--- hello.py
+++ hello.py
@@ -1,12 +1,26 @@
-from flask import Flask
-
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
+    return render_template('index.html',
+                       current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
